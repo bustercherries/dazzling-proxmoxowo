306a00724814190f253806a46df99377dd826a23
Róża <jackowska.roza23@gmail.com>
2026-09-02 15:09:14 +0200

    configuration basic template

---
+++ post_VM_creation.sh
@@ -0,0 +1,10 @@
+### Variables
+
+### 1. User app creation
+### 2. Install docker + compose
+### 3. Set up Immich directory and files
+### 4. Get docker-compose.yml and .env tempalte
+
+# Configure .env
+
+### Start immich
